128dadfca5d91409fd7a78af15e8b93693fc2c87
Lanu135 <143382856+Lanu135@users.noreply.github.com>
2024-05-12 18:45:44 +0200

    tasks will be added to html dynamically, also they can be removed

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const addTaskElement = document.getElementById('add-task')
 const addTaskBtn = document.getElementById('add-task-btn')
+const openTaskWrapper = document.querySelector('.open')
+
+const ul = document.getElementById('ul')
 
 const openTasks = []
 
@@ -9,5 +12,30 @@ addTaskBtn.addEventListener('click', () => {
   } else {
     openTasks.push(addTaskElement.value)
     addTaskElement.value = ''
+    manageTasks()
   }
 })
+
+// adds and removes tasks from the DOM
+const manageTasks = () => {
+  const li = document.createElement('li')
+  const xIcon = document.createElement('i')
+  xIcon.setAttribute('class', 'fa-solid fa-xmark')
+  const deleteBtn = document.createElement('button')
+  deleteBtn.setAttribute('class', 'delete-task')
+  for (let i = 0; i < openTasks.length; i++) {
+    li.setAttribute('id', i)
+    deleteBtn.setAttribute('id', i)
+    li.innerText = openTasks[i]
+    deleteBtn.appendChild(xIcon)
+    li.appendChild(deleteBtn)
+    ul.appendChild(li)
+
+    deleteBtn.addEventListener('click', (e) => {
+      if (e.target.id == i) {
+        openTasks.splice(i, 1)
+        ul.removeChild(li)
+      }
+    })
+  }
+}
